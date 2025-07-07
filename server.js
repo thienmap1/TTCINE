62ab2db7c46431323a63adcda40d6cb5c84eed0e
thienmap1 <thienmap094@gmail.com>
2025-07-07 21:37:47 +0700

    ba

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => {
     console.log(' MongoDB connected');
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(` Server running on http://localhost:${PORT}`);
-    });
+    app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${{PORT}}`);
+});
   })
   .catch((err) => {
     console.error(' MongoDB connection error:', err.message);
